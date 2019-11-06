f2de7e055950f95bb1b1a554f3ee56b10f4e478b
Steven Quirion <SQuirion@ContactAAC.com>
2019-11-06 14:45:54 -0600

    upload

---
+++ Public/Get-Five9AdminClient.ps1
@@ -0,0 +1,19 @@
+﻿function Get-Five9AdminClient
+{
+    param
+    (
+        [Parameter(Mandatory=$true)][string]$Username,
+        [Parameter(Mandatory=$true)][string]$Password
+    )
+
+    $wsdl = "https://api.five9.com/wsadmin/v11/AdminWebService?wsdl&user=$Username"
+    $proxy = New-WebServiceProxy -Uri $wsdl
+    $proxy.Credentials = $(New-Object System.Net.NetworkCredential($Username, $Password))
+
+    return $proxy
+
+}
+
+
+
+
