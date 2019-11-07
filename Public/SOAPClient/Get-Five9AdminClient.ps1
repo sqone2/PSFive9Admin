e94f660560a9b7df643cca1eaf8cd36c9002bfac
Steven Quirion <SQuirion@ContactAAC.com>
2019-11-07 08:31:22 -0600

    adding functions

--- Public/SOAPClient/Get-Five9AdminClient.ps1
+++ Public/SOAPClient/Get-Five9AdminClient.ps1
@@ -6,14 +6,23 @@
         [Parameter(Mandatory=$true)][string]$Password
     )
 
+    # get soap client
     $wsdl = "https://api.five9.com/wsadmin/v11/AdminWebService?wsdl&user=$Username"
     $proxy = New-WebServiceProxy -Uri $wsdl -Namespace "PSFive9Admin"
     $proxy.Credentials = $(New-Object System.Net.NetworkCredential($Username, $Password))
 
+
+    # test credentails
+    try
+    {
+        $test = $proxy.getUserGeneralInfo($Username)
+    }
+    catch
+    {
+        throw "Error connecting to Five9 Web Service. Please check your credentials and try again. $($_.Exception.Message)"
+    }
+
     return $proxy
 
 }
-
-
-
 
---
+++ Public/Skills/Get-Five9Skills.ps1
@@ -0,0 +1,57 @@
+﻿<#
+.SYNOPSIS
+    
+    Function used to get Skill objects from Five9
+ 
+.DESCRIPTION
+ 
+    Function used to get Skill objects from Five9
+ 
+.PARAMETER Five9AdminClient
+ 
+    Mandatory parameter. SOAP Proxy Client Object. Use function "Get-Five9AdminClient" to get SOAP client
+
+.PARAMETER AllSkills
+ 
+    Returns all skills in Five9  domain using .* regex pattern
+
+.PARAMETER NamePattern
+ 
+    Returns only skills matching a given regex string
+   
+.EXAMPLE
+    
+    $adminClient = Get-Five9AdminClient -Username "user@domain.com" -Password "P@ssword!"
+    Get-Five9Skill -Five9AdminClient $adminClient -AllSkills: $true
+    
+    # Returns all skills in Five9 domain
+    
+.EXAMPLE
+    
+    $adminClient = Get-Five9AdminClient -Username "user@domain.com" -Password "P@ssword!"
+    Get-Five9Skill -Five9AdminClient $adminClient -NamePattern "MultiMedia"
+    
+    # Returns all skills matching the string "MultiMedia"
+    
+
+ 
+#>
+
+function Get-Five9Skills
+{
+    [CmdletBinding(DefaultParametersetName='AllSkills')] 
+    param
+    ( 
+        [Parameter(Mandatory=$true)][object]$Five9AdminClient,      
+        [Parameter(ParameterSetName='AllSkills',Mandatory=$true)][switch]$AllSkills,
+        [Parameter(ParameterSetName='NamePattern',Mandatory=$true)][ValidateNotNullOrEmpty()][string]$NamePattern
+    )
+
+    if ($PsCmdLet.ParameterSetName -eq "All")
+    {
+        $NamePattern = '.*'
+    }
+    
+    return $Five9AdminClient.getSkills($NamePattern)
+
+}
