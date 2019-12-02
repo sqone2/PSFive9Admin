0f2e1033f0857937fb7ccd4986c05cf394c587fb
Steve Quirion <steveq86@yahoo.com>
2019-12-02 10:02:05 -0600

    udpate

---
+++ Public/Campaigns/Reset-Five9Campaign.ps1
@@ -0,0 +1,32 @@
+﻿<#
+.SYNOPSIS
+    
+    Function to reset a campaign to redial every number, except for numbers on the Do-Not-Call list
+ 
+.PARAMETER Five9AdminClient
+ 
+    Mandatory parameter. SOAP Proxy Client Object. Use function "New-Five9AdminClient" to get SOAP client
+
+.PARAMETER CampaignName
+ 
+    Campaign name to be reset
+
+.EXAMPLE
+    
+    $adminClient = New-Five9AdminClient -Username "user@domain.com" -Password "P@ssword!"
+    Reset-Five9Campaign -Five9AdminClient $adminClient -CampaignName 'Hot-Leads'
+
+    # resets campaign named 'Hot-Leads'
+
+#>
+function Reset-Five9Campaign
+{
+    param
+    ( 
+        [Parameter(Mandatory=$true)][PSFive9Admin.WsAdminService]$Five9AdminClient,
+        [Parameter(Mandatory=$true)][string]$CampaignName
+    )
+
+    return $Five9AdminClient.resetCampaign($CampaignName)
+
+}
