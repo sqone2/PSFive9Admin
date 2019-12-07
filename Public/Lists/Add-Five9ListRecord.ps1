e6cda894d2550e4ddae0e5211f70d51198d96892
Steve Quirion <steveq86@yahoo.com>
2019-12-07 06:55:55 -0600

    udpate

--- Public/Lists/Add-Five9ListRecord.ps1
+++ Public/Lists/Add-Five9ListRecord.ps1
@@ -1,41 +1,111 @@
 ﻿<#
 .SYNOPSIS
     
-    Function used to delete a new Five9 list
- 
-.DESCRIPTION
- 
-    Function used to delete a new Five9 list
+    Function used to add a single record, or an array of objects an outbound dialing list.
  
 .PARAMETER Five9AdminClient
  
     Mandatory parameter. SOAP Proxy Client Object. Use function "New-Five9AdminClient" to get SOAP client
 
-.PARAMETER Name
+.PARAMETER ListName
 
-    Name of new list to be removed
+    Name of list that records will be added to
+
+.PARAMETER InputObject
+
+    Single object or array of objects to be added to list. Note: Parameter not needed when specifying a CsvPath
+
+.PARAMETER CsvPath
+    
+    Local file path to CSV file. Note: Parameter not needed when specifying an InputObject
+
+.PARAMETER CrmAddMode
+
+    Specifies whether a contact record is added to the contact database when a new record is added to a dialing list.
+
+    Options are:
+        • ADD_NEW (Default) - Contact records are created in the contact database and are added to the dialing list
+        • DONT_ADD - Records are added to the dialing list but no records are created in the contact database
+
+.PARAMETER CrmUpdateMode
+
+    Specifies how contact records should be updated when records are added to a dialing list.
+
+    Options are:
+        • UPDATE_SOLE_MATCHES (Default) - Update only if one matched record is found
+        • UPDATE_FIRST - Update the first matched record
+        • UPDATE_ALL - Update all matched records
+        • DONT_UPDATE - Do not update any record
+
+.PARAMETER ListAddMode
+
+    Specifies how to add records to a list
+
+    Options are:
+        • ADD_IF_SOLE_CRM_MATCH (Default) - Add record if only one match exists in the database
+        • ADD_FIRST - Adds the first record when multiple matches exist
+        • ADD_ALL - Add all records
+        
+
+.PARAMETER Key
+
+    Single string, or array of strings which designate key(s). Used when a record needs to be updated, it is used to find the record to update in the contact database.
+    If omitted, 'number1' will be used
+
+.PARAMETER CleanListBeforeUpdate
+
+    Whether to remove all records in the list before adding new records. If set to True, all existing records will be removed from list before being udpated
+
+.PARAMETER FailOnFieldParseError
+
+    Whether to stop the import if incorrect data is found
+    For example, if set to True and you have a column named hair_color in your data, but that field has not been created as a contact field, the list import will fail
+
+    Options are:
+    • True: The record is rejected when at least one field fails validation
+    • False: Default. The record is accepted. However, changes to the fields that fail validation are rejected
+
+
+.PARAMETER ReportEmail
+
+    Notification about import results is sent to the email addresses that you set for your application
    
 .EXAMPLE
     
     $adminClient = New-Five9AdminClient -Username "user@domain.com" -Password "P@ssword!"
-    Remove-Five9List -Five9AdminClient $adminClient -Name "Cold-Call-List"
+    Add-Five9ListRecord -Five9AdminClient $adminClient -ListName "Hot-Leads" -InputObject $dataToBeImported
 
-    # Deletes list named "Cold-Call-List"
+    # Records in $dataToBeImported will be imported into Five9 list named "Hot-Leads" using default values
 
- 
+.EXAMPLE
+
+    Add-Five9ListRecord -Five9AdminClient $adminClient -ListName "Hot-Leads" -CsvPath C:\files\list-data.csv
+
+    # Records in CSV file "C:\files\list-data.csv"  will be imported into Five9 list named "Hot-Leads" using default values
+
+.EXAMPLE
+
+    Add-Five9ListRecord -Five9AdminClient $adminClient -ListName "Hot-Leads" -CsvPath C:\files\list-data.csv `
+                        -CrmAddMode: ADD_NEW -CrmUpdateMode: UPDATE_ALL -ListAddMode: ADD_ALL -Key @('number1', 'first_name') `
+                        -CleanListBeforeUpdate: $true -FailOnFieldParseError $true -ReportEmail 'jdoe@domain.com'
+
+    # Importing CSV file to list, specifying additional optional parameters
+
+
 #>
 function Add-Five9ListRecord
 {
-    [CmdletBinding(PositionalBinding=$false)]
+    [CmdletBinding(DefaultParametersetName='InputObject', PositionalBinding=$false)]
     param
     ( 
         [Parameter(Mandatory=$true)][PSFive9Admin.WsAdminService]$Five9AdminClient,
         [Parameter(Mandatory=$true)][string]$ListName,
-        [Parameter(Mandatory=$true)][psobject[]]$InputObject,
+        [Parameter(ParameterSetName='InputObject', Mandatory=$true)][psobject[]]$InputObject,
+        [Parameter(ParameterSetName='CsvPath', Mandatory=$true)][string]$CsvPath,
+        [Parameter(Mandatory=$false)][string][ValidateSet("ADD_NEW", "DONT_ADD")]$CrmAddMode = "ADD_NEW",
+        [Parameter(Mandatory=$false)][string][ValidateSet("UPDATE_FIRST", "UPDATE_ALL", "UPDATE_SOLE_MATCHES", "DONT_UPDATE")]$CrmUpdateMode = "UPDATE_SOLE_MATCHES",
+        [Parameter(Mandatory=$false)][string][ValidateSet("ADD_FIRST", "ADD_ALL", "ADD_IF_SOLE_CRM_MATCH")]$ListAddMode = "ADD_IF_SOLE_CRM_MATCH",
         [Parameter(Mandatory=$false)][string[]]$Key = @("number1"),
-        [Parameter(Mandatory=$true)][string][ValidateSet("ADD_NEW", "DONT_ADD")]$CrmAddMode,
-        [Parameter(Mandatory=$true)][string][ValidateSet("UPDATE_FIRST", "UPDATE_ALL", "UPDATE_SOLE_MATCHES", "DONT_UPDATE")]$CrmUpdateMode,
-        [Parameter(Mandatory=$true)][string][ValidateSet("ADD_FIRST", "ADD_ALL", "ADD_IF_SOLE_CRM_MATCH")]$ListAddMode,
         [Parameter(Mandatory=$false)][bool]$CleanListBeforeUpdate,
         [Parameter(Mandatory=$false)][bool]$FailOnFieldParseError,
         [Parameter(Mandatory=$false)][string]$ReportEmail
@@ -45,13 +115,28 @@ function Add-Five9ListRecord
 
     try
     {
-        $csv = $InputObject | ConvertTo-Csv -NoTypeInformation
+        if ($PSCmdlet.ParameterSetName -eq 'InputObject')
+        {
+            $csv = $InputObject | ConvertTo-Csv -NoTypeInformation
+        }
+        elseif ($PSCmdlet.ParameterSetName -eq 'CsvPath')
+        {
+            # try to import csv file so that if it throw an error, we know the data is bad
+            $csv = Import-Csv $CsvPath | ConvertTo-Csv -NoTypeInformation
+        }
+        else
+        {
+            # should never reach this point becasue user should use either InputObject or CsvPath
+            return
+        }
     }
     catch
     {
         throw $_
         return
     }
+    
+
 
 
     $headers = $csv[0] -replace '"' -split ','
@@ -59,9 +144,9 @@ function Add-Five9ListRecord
     # verify that key(s) passed are present in $Inputobject
     foreach ($k in $Key)
     {
-        if ($headers -notcontains $key)
+        if ($headers -notcontains $k)
         {
-            throw "Specified key ""$k"" is not a property name found in -InputObject."
+            throw "Specified key ""$k"" is not a property name found in data being imported."
             return
         }
     }
@@ -134,8 +219,8 @@ function Add-Five9ListRecord
     }
 
 
-
-
-
 }
 
+
+
+Add-Five9ListRecord -Five9AdminClient $demoFive9AdminClient -ListName "_Preview_Test" -CsvPath C:\Users\squirion\scripts\Working\list-import-test.csv -CrmAddMode: ADD_NEW -CrmUpdateMode: UPDATE_ALL -ListAddMode: ADD_ALL -Key @('number1', 'first_name') -CleanListBeforeUpdate: $true -FailOnFieldParseError $true -ReportEmail 'squirion@five9.com'
