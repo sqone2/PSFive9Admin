49a278f88db580cf395c0ce5035cbeed2f09bdc8
Steve Quirion <steveq86@yahoo.com>
2019-12-23 08:38:55 -0600

    udpate

--- Public/CallVariables/New-Five9CallVariable.ps1
+++ Public/CallVariables/New-Five9CallVariable.ps1
@@ -1,140 +1,105 @@
-﻿<#
-.SYNOPSIS
-    
-    Function used to modify existing call variable
-
-.PARAMETER Name
-
-    Name of existing call variable
-
-.PARAMETER Group
-
-    Group name of existing call variable
-
-.PARAMETER Description
-
-    Description for new call variable
-
-.PARAMETER Type
-
-    Type of data stored in this field
-    Options are:
-        • STRING (Default) - Letters and numbers
-        • NUMBER - Numbers only
-        • DATE - Date only
-        • TIME - Time only
-        • DATE_TIME - Date and time
-        • CURRENCY - Currency
-        • BOOLEAN - True or false
-        • PERCENT - Percentage
-        • EMAIL - Email address
-        • URL - URL
-        • PHONE - Phone number
-        • TIME_PERIOD - Time interval (Duration)
-
-.PARAMETER ApplyToAllDispositions
-
-    If set to $true, variable will be set for all dispositions
-
-.PARAMETER Dispositions
-
-    If -ApplyToAllDispositions is $false, this parameter lists the names of the dispositions for which to set this variable
-
-.PARAMETER Reporting
-
-    Whether to add the values to reports
-
-
-.PARAMETER DefaultValue
-
-    Optional value that may be assigned to a call variable. For example, a boolean's default value could be set to "True" or "False"
-
-
-.PARAMETER PredefinedList
-
-    Single string, or array of multiple strings which are the only possible values for this field to be set to
-
-.PARAMETER CanSelectMultiple
-
-    Whether multiple values from PredefinedList can be selected as value
-
-.PARAMETER MinValue
-
-    Minimum value
-
-.PARAMETER MaxValue
-
-    Maximum value
-
-.PARAMETER Regexp
-    
-    Regular expression that field value must match
-
-.PARAMETER DigitsBeforeDecimal
-
-    Digits before decimal point
-
-.PARAMETER DigitsAfterDecimal
-
-    Digits after decimal point
-
-.PARAMETER TimeFormat
-
-    Time format string. i.e. yyyy-MM-dd
-
-.PARAMETER DateFormat
-
-    Time format string. i.e. HH:mm:ss.SSS
-
-.PARAMETER TimePeriodFormat
-
-    Time format string. i.e. hh:mm:ss.SSS
-
-.PARAMETER CurrencyType
-
-    Type of currency
-    Options are:
-        • Dollar
-        • Euro
-        • Pound
-
-   
-
-.EXAMPLE
-
-    Set-Five9CallVariable -Name "MiddleName" -Group "CustomerVars" -ApplyToAllDispositions $true -Reporting $true
-
-    # Modifies existing call variable named "MiddleName" within the "CustomerVars" call variable group
-    
-#>
-
-function New-Five9CallVariable
+﻿function New-Five9CallVariable
 {
+    <#
+    .SYNOPSIS
+    
+        Function used to modify existing call variable
+
+    .EXAMPLE
+
+        Set-Five9CallVariable -Name "MiddleName" -Group "CustomerVars" -ApplyToAllDispositions $true -Reporting $true
+
+        # Modifies existing call variable named "MiddleName" within the "CustomerVars" call variable group
+    
+    #>
+
     [CmdletBinding(PositionalBinding=$false)]
     param
     ( 
-        [Parameter(Mandatory=$true)][string]$Name,
-        [Parameter(Mandatory=$true)][string]$Group,
+        # Name of existing call variable
+        [Parameter(Mandatory=$true, Position=0)][string]$Name,
+
+        # Group name of existing call variable
+        [Parameter(Mandatory=$true, Position=1)][string]$Group,
+
+        # Description for new call variable
         [Parameter(Mandatory=$false)][string]$Description,
+
+        <#
+        Type of data stored in this field
+
+        Options are:
+            • STRING (Default) - Letters and numbers
+            • NUMBER - Numbers only
+            • DATE - Date only
+            • TIME - Time only
+            • DATE_TIME - Date and time
+            • CURRENCY - Currency
+            • BOOLEAN - True or false
+            • PERCENT - Percentage
+            • EMAIL - Email address
+            • URL - URL
+            • PHONE - Phone number
+            • TIME_PERIOD - Time interval (Duration)
+        #>
         [Parameter(Mandatory=$false)][ValidateSet('STRING', 'NUMBER', 'DATE', 'TIME', 'DATE_TIME', 'CURRENCY', 'BOOLEAN', 'PERCENT', 'EMAIL', 'URL', 'PHONE', 'TIME_PERIOD')][string]$Type = "STRING",
+
+        # If set to $true, variable will be set for all dispositions
         [Parameter(Mandatory=$false)][bool]$ApplyToAllDispositions = $false,
+
+        # If -ApplyToAllDispositions is $false, this parameter lists the names of the dispositions for which to set this variable
         [Parameter(Mandatory=$false)][string[]]$Dispositions,
+
+        # Whether to add the values to reports
         [Parameter(Mandatory=$false)][bool]$Reporting,
+
+        # Default initial value assigned to call variable
         [Parameter(Mandatory=$false)][string]$DefaultValue,
+
+        # Whether the variable contains personal data that identifies the customer
         [Parameter(Mandatory=$false)][bool]$SensitiveData,
 
-        # Restrictions
+        # Whether the field must contain a value
         [Parameter(Mandatory=$false)][bool]$Required,
-        [Parameter(Mandatory=$false)][string[]]$PredefinedList, #Set
-        [Parameter(Mandatory=$false)][bool]$CanSelectMultiple, #Multiset
+
+        # Single string, or array of multiple strings which are the only possible values for this field to be set to
+        [Parameter(Mandatory=$false)][string[]]$PredefinedList,
+
+        # Whether multiple values from PredefinedList can be selected as value
+        [Parameter(Mandatory=$false)][bool]$CanSelectMultiple,
+
+        # Minimum value
         [Parameter(Mandatory=$false)][string]$MinValue,
+
+        # Maximum value
         [Parameter(Mandatory=$false)][string]$MaxValue,
+
+        # Regular expression that field value must match
         [Parameter(Mandatory=$false)][string]$Regexp,
-        [Parameter(Mandatory=$false)][int]$DigitsBeforeDecimal, #Scale
-        [Parameter(Mandatory=$false)][int]$DigitsAfterDecimal, #Precision
+
+        # Digits before decimal point
+        [Parameter(Mandatory=$false)][int]$DigitsBeforeDecimal,
+
+        # Digits after decimal point
+        [Parameter(Mandatory=$false)][int]$DigitsAfterDecimal,
+
+        # Time format string. i.e. yyyy-MM-dd
         [Parameter(Mandatory=$false)][ValidateSet('HH:mm:ss.SSS', 'HH:mm:ss', 'HH:mm', 'hh:mm a', 'HH', 'hh a', 'H:mm', 'h:mm a')][string]$TimeFormat,
+
+        # Time format string. i.e. HH:mm:ss.SSS
         [Parameter(Mandatory=$false)][ValidateSet('yyyy-MM-dd', 'MM/dd/yyyy', 'MM-dd-yyyy', 'MM-dd-yy', 'MMM dd', 'yyyy', 'dd MMM', 'dd-MM', 'MM-dd')][string]$DateFormat,
+
+        # Time format string. i.e. hh:mm:ss.SSS
         [Parameter(Mandatory=$false)][ValidateSet('hh:mm:ss.SSS', 'hh:mm:ss', 'hh:mm', 'hh', 'mm:ss.SSS', 'mm:ss', 'mm', 'ss.SSS', 'ss', 'SSS')][string]$TimePeriodFormat,
+
+        <# 
+        Type of currency
+
+        Options are:
+             • Dollar
+             • Euro
+             • Pound
+        #>
         [Parameter(Mandatory=$false)][ValidateSet('Dollar', 'Pound', 'Euro')][string]$CurrencyType
     )
 
