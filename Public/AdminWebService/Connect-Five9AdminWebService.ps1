d7ec1b36aacf669579bb02f8d019bccb146c4fbb
Steve Quirion <sqone2@gmail.com>
2023-07-11 19:05:48 -0400

    added local wsdl file

--- Public/AdminWebService/Connect-Five9AdminWebService.ps1
+++ Public/AdminWebService/Connect-Five9AdminWebService.ps1
@@ -2,19 +2,19 @@
 {
     <#
     .SYNOPSIS
-    
+
         Function used to create a web service proxy with the Five9 admin web service
 
     .EXAMPLE
-    
+
         Connect-Five9AdminWebService
-    
+
         # User will be prompted to enter Five9 username and password, and then be connected to Five9 admin web service
 
     .EXAMPLE
-    
+
         Connect-Five9AdminWebService -DataCenter "EU"
-    
+
         # Domain being connected to is loacated in the Five9 EU data center
 
     .EXAMPLE
@@ -23,22 +23,22 @@
         $password = 'P@ssword!' | ConvertTo-SecureString -AsPlainText -Force
         $cred = New-Object -TypeName PSCredential -ArgumentList $username,$password
         Connect-Five9AdminWebService -Credential $cred
-    
+
         # Create PSCredential object and connects to Five9 admin web service
 
     #>
-    [CmdletBinding(PositionalBinding=$false)]
+    [CmdletBinding(PositionalBinding = $false)]
     param
     (
         # PSCredential object, such as one returned by the Get-Credential cmdlet.
         # If parameter is omitted, Get-Credential will be called.
-        [Parameter(Mandatory=$false, Position=0)][PSCredential]$Credential = (Get-Credential -Message "Please enter your Five9 admin credentials"),
+        [Parameter(Mandatory = $false, Position = 0)][PSCredential]$Credential = (Get-Credential -Message "Please enter your Five9 admin credentials"),
 
-        # Optional API version. See Five9 documentation for details on what is provided with each version. 
+        # Optional API version. See Five9 documentation for details on what is provided with each version.
         # If omitted, most recent version will be used (recommended).
-        [Parameter(Mandatory=$false)][string]$Version = '12',
+        [Parameter(Mandatory = $false)][string]$Version = '12',
 
-        <# 
+        <#
         Data center that contains the Five9 domain you are connecting to
 
         Options are:
@@ -47,10 +47,10 @@
             • EU_Frankfurt - European Union data center (Frankfurt) - api.eu.five9.com
             • Canada - Canada Data center - api.five9.ca
         #>
-        [Parameter(Mandatory=$false)][ValidateSet('US', 'EU', 'EU_Frankfurt', 'Canada')][string]$DataCenter = 'US',
+        [Parameter(Mandatory = $false)][ValidateSet('US', 'EU', 'EU_Frankfurt', 'Canada')][string]$DataCenter = 'US',
 
         # Returns an object that contains the web service proxy
-        [Parameter(Mandatory=$false)][switch]$PassThru = $false
+        [Parameter(Mandatory = $false)][switch]$PassThru = $false
     )
 
 
@@ -64,14 +64,14 @@
             return
         }
 
-        
+
         if ($DataCenter -eq 'US')
         {
             $baseUrl = 'api.five9.com'
         }
         if ($DataCenter -eq 'EU')
         {
-            $baseUrl = "api.five9.eu"            
+            $baseUrl = "api.five9.eu"
         }
         elseif ($DataCenter -eq 'EU_Frankfurt')
         {
@@ -92,7 +92,14 @@
 
         Write-Verbose "Connecting to: $($wsdl)"
 
-        $global:DefaultFive9AdminClient = New-WebServiceProxy -Uri $wsdl -Namespace "PSFive9Admin" -Class "PSFive9Admin" -ErrorAction: Stop
+        try
+        {
+            $global:DefaultFive9AdminClient = New-WebServiceProxy -Uri $wsdl -Namespace "PSFive9Admin" -Class "PSFive9Admin" -ErrorAction: Stop
+        }
+        catch
+        {
+            $global:DefaultFive9AdminClient = New-WebServiceProxy "$PSScriptRoot/Public/assets/WSDL/Five9Admin.wsdl" -Namespace "PSFive9Admin" -Class "PSFive9Admin" -ErrorAction: Stop
+        }
 
         $global:DefaultFive9AdminClient.Credentials = $Credential
         $global:DefaultFive9AdminClient.Credentials.UserName = $Credential.UserName
@@ -111,7 +118,7 @@
         throw "Error creating web service proxy to Five9 Admin Web Service. $($_.Exception.Message)"
         return
     }
-    
+
     # test credentails
     try
     {
